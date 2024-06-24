142ce7a1fd57522858beffba804b34f3b3d3e45c
k-code-c <153966637+k-code-c@users.noreply.github.com>
2024-06-24 09:55:57 -0400

    Add files via upload
    This was the first project I did during my freshmen year of college where the goal was to Write a C++ program that computes a Total of 5 numbers {12098, 30982, 10983, 8983, 20392} and The percentage each number represents of the total number. I am just going off of the description at hand for the assignment.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+ 
+    int num1 = 34567; //declartion
+    int num2 = 86437;
+    int num3 = 98425;
+    int num4 = 38124;
+    int num5 = 23576;
+    double total = num1 + num2 + num3 + num4 + num5;
+    
+    cout << "The computed value is = " << total << endl;
+    
+    cout << "34567 is " << (num1 / total) * 100 << "%" << " of 281,129" << endl; //information is read from the keyboard into the program
+    
+    cout << "86437 is " << (num2 / total) * 100 << "%" << " of 281,129" << endl;
+    
+    cout << "98425 is " << (num3 / total) * 100 << "%" << " of 281,129" << endl;
+    
+    cout << "38124 is " << (num4 / total) * 100 << "%" << " of 281,129" << endl;
+    
+    cout << "23576 is " << (num5 / total) * 100 << "%" << " of 281,129" << endl;
+    
+    cout << "KC Ashiowu Homwork1";
+    
+    return 0;
+}
+
+
+
+
+
